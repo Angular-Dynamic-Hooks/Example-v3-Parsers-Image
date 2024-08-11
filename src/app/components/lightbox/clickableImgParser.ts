4c6bd0e40b96a450e44bc1828d917ee04f37671f
Marvin <Mvin@live.de>
2024-08-11 21:04:57 +0200

    Simplified parser method signatures

--- src/app/components/lightbox/clickableImgParser.ts
+++ src/app/components/lightbox/clickableImgParser.ts
@@ -7,19 +7,19 @@ import { ClickableImgComponent } from './clickableImg.component';
 })
 export class ClickableImgParser implements HookParser {
 
-  findHookElements(contentElement: any, context: any, options: ParseOptions): any[] {
+  findHookElements(contentElement: any): any[] {
     // Find all img-elements with the lightbox class
     return Array.from(contentElement.querySelectorAll('img.lightbox'));
   }
 
-  loadComponent(hookId: number, hookValue: HookValue, context: any, childNodes: any[], options: ParseOptions): HookComponentData {
+  loadComponent(): HookComponentData {
     return {
       component: ClickableImgComponent,   // Load our component
       hostElementTag: 'lightbox-img'      // As img-elements can't have content, replace them with '<lightbox-img>' elements
     };
   }
 
-  getBindings(hookId: number, hookValue: HookValue, context: any, options: ParseOptions): HookBindings {
+  getBindings(hookId: number, hookValue: HookValue): HookBindings {
     // Read the image urls from the element attributes and pass along as inputs
     const imgElement: HTMLImageElement = hookValue.elementSnapshot;
     return {
